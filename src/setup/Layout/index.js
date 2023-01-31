f08f9be318c55a6c2dea01b40a0911d88e48cd03
argezdev <lrcuellar@unitecnica.net>
2023-01-31 11:21:33 -0500

    Test

--- src/setup/Layout/index.js
+++ src/setup/Layout/index.js
@@ -14,7 +14,6 @@ const Layout = () => {
     setIsOpen((prevState) => !prevState);
   };
 
-  // TODO: ver si hay una mejor manera de hacer esto
   const pathName = location.pathname.split('/');
   const isCheckout = pathName.includes('checkout');
 
